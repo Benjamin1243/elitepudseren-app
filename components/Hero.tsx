f88beec9327d2098c9517bf75b12e7be8c517525
Benjamin1243 <U2486886@rts-365.dk>
2025-12-18 17:53:28 +0100

    fixed header plus fixed companys we help section changed gap

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,9 +33,9 @@ console.log("Image data", data)
     return( 
         <div className={`  grid h-screen sm:h-[90vh] bg-cover bg-center h-64    md:[clip-path:polygon(0_0,100%_0,100%_85%,0_100%)]`} style={{backgroundImage: `url("${ data.data[0].local ? data.data?.[0].hero.backgroundImg.url: data.data?.[0].hero.backgroundImg.url}")`}}>
             <div className=" z-[1] blur-1xl bg-black opacity-50 col-span-full row-span-full"></div>
-            <div className=" ml-10 col-span-full row-span-full z-10 self-center  max-w-[80%] md:max-w-[45%]">
+            <div className=" ml-10 col-span-full row-span-full z-10 self-center  max-w-[90%] md:max-w-[45%]">
             <h1 className="  text-background text-5xl md:text-6xl ">{data.data?.[0]?.hero?.overskrift}</h1>
-            <p className=" text-background mt-5"> {data.data?.[0].hero.underskrift}</p>
+            <p className=" text-background max-w-[80%] mt-5"> {data.data?.[0].hero.underskrift}</p>
             <div  className=" flex gap-5 mt-3.5">
             {data.data?.[0]?.hero?.heroButton?.map((button: HeroButton)=>{
                 return ( <LinkButton key={button.id + "link"} background={button.background} text={button.text} href={button.href}></LinkButton>)
--- components/PartnersShow.tsx
+++ components/PartnersShow.tsx
@@ -20,7 +20,7 @@ export default async function PartnersShow(){
         <article className=" md:max-w-1/2 mt-10">
             <h2 className=" text-3xl font-bold text-center font-open-sans ">Virksomheder der stoler på os</h2>
             <hr className=" my-5 border-[1px] rounded-2xl" />
-            <div className=" grid-cols-3 w-11/12 mx-auto grid gap-5">
+            <div className=" grid-cols-3 w-11/12 mx-auto grid gap-3 md:gap-5">
                 {data.data?.[0]?.partners?.map((partner: Partner)=>{
                     return ( <PartnerComponent key={partner.id} url={partner.image.url}></PartnerComponent>)
                 })}
